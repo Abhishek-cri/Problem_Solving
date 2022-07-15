002393a62acce57ede2b10b3066e4dccb852cfc1
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-07-15 23:03:59 +0530

    Added solution - LeetHub

---
+++ Binary Array Sorting - GFG/binary-array-sorting.c
@@ -0,0 +1,64 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+// A[]: input array
+// N: input array
+//Function to sort the binary array.
+void binSort(int A[], int n)
+{
+   //Your code here
+   
+   /**************
+    * No need to print the array
+    * ************/
+    int count=0;
+    
+    for(int i=0;i<n;i++)
+    {
+        if(A[i]==0)
+        {
+            count+=1;
+        }
+    }
+    
+    for(int i=0;i<count;i++)
+    {
+        A[i]=0;
+    }
+    
+    for(int i=count;i<n;i++)
+    {
+        A[i]=1;
+    }
+}
+
+// { Driver Code Starts.
+
+int main() {
+	int T;
+	scanf("%d", &T);
+	// Input the number of testcases
+	while(T--)
+	{
+	    int N;
+	    scanf("%d", &N); //Input size of array N
+	    int A[N]; 
+	    
+	    for(int i = 0; i  < N; i++)
+	      scanf("%d", &A[i]);
+	      
+	    binSort(A,N);
+	    
+	    for(int i = 0; i  < N; i++)
+	        printf("%d ", A[i]);
+	      
+	    printf("\n");
+	}
+	return 0;
+}  // } Driver Code Ends
