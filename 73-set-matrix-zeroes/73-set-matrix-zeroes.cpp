de101dbd5eb23adb4524c1b0f7b07733ef1e0f96
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-08-20 18:02:33 +0530

    Time: 21 ms (60.98%), Space: 13.4 MB (10.54%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.cpp
@@ -2,27 +2,38 @@ class Solution {
 public:
     void setZeroes(vector<vector<int>>& matrix) {
         
-        vector<vector<int>>temp=matrix;
+        set<int>a;
+        set<int>b;
         
         for(int i=0;i<matrix.size();i++)
         {
             for(int j=0;j<matrix[0].size();j++)
             {
-                 if(matrix[i][j]==0)
-                 {
-                     for(int i=0;i<matrix.size();i++)
-                     {
-                         temp[i][j]=0;
-                     }
-                     for(int j=0;j<matrix[0].size();j++)
-                     {
-                         temp[i][j]=0;
-                     }
-                 }
+                if(matrix[i][j]==0)
+                {
+                    a.insert(i);
+                    b.insert(j);
+                }
             }
         }
         
-        matrix=temp;
+        for(int i=0;i<matrix.size();i++)
+        {
+            for(int j=0;j<matrix[0].size();j++)
+            {
+                
+                // cout<<a.count(i)<<" ";
+                //  cout<<endl<<b.count(j)<<" "<<endl;
+                
+                if(a.count(i)|| b.count(j))
+                {
+                    matrix[i][j]=0;
+                }
+            }
+        }
+        
+        
+        
         
     }
 };
