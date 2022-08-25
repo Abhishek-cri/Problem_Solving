e821c28c179029cf0a0fdb8f95f5c6243e057498
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-08-25 11:07:33 +0530

    Time: 17 ms (56.25%), Space: 7.1 MB (11.69%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -5,17 +5,16 @@ public:
         int left=0;
         int right=0;
         int count=INT_MIN;
-        int ans=0;
         unordered_map<char,int>mp;
+        int ans=0;
         while(right<s.size())
         {
             mp[s[right]]++;
+            count=max(count,mp[s[right]]);
             
-            count=max(count, mp[s[right]]);
-            
-            if(((right-left)+1)-count<=k)
+            if((right-left)+1-count<=k)
             {
-                ans=max(ans,(right-left)+1);    // window_len=(right-left)+1
+                ans=max(ans,(right-left)+1);
             }
             else
             {
@@ -24,7 +23,6 @@ public:
             }
             right++;
         }
-        
         return ans;
     }
 };
