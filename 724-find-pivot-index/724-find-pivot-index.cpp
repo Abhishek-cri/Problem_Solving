4b8da071adeb37da5715f550849450b75bc59fab
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-09-30 04:35:28 +0530

    Time: 64 ms (16.90%), Space: 31.1 MB (87.11%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -3,18 +3,21 @@ public:
     int pivotIndex(vector<int>& nums) {
         
         int sum=accumulate(nums.begin(), nums.end(),0);
-        int cur=0;
         
+        int curr=0;
         for(int i=0;i<nums.size();i++)
         {
-            int temp=cur;
-            cur+=nums[i];
-            if(sum-cur==temp)
+        
+            int temp=curr;
+            curr+=nums[i];
+            if(sum-curr==temp)
             {
                 return i;
             }
         }
+        
         return -1;
+        
         
     }
 };
