ca49e25a165ec020ae1ebe7d2eae70f95318c6f9
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-09-30 11:19:23 +0530

    Time: 6 ms (85.83%), Space: 7.2 MB (19.86%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -2,33 +2,33 @@ class Solution {
 public:
     bool isIsomorphic(string s, string t) {
         
-        
         int n=s.size();
         int m=t.size();
         
-        if(n!=m)
-        {
-            return false;
-        }
-        map<char, char>mp1,mp2;
-       for(int i=0;i<n;i++)
+       if(n!=m)
        {
-           if(mp1.find(s[i])==mp1.end() && mp2.find(t[i])==mp2.end())
-           {
-               mp1[s[i]]=t[i];
-               mp2[t[i]]=s[i];
-           }
-        else{   
-          if(mp2[t[i]]!=s[i] || mp1[s[i]]!=t[i])
-          {
-              return false;
-          }
-         }
-           
-       }     
-      return true; 
+           return false;
+       }
         
+      map<char, char>mp1, mp2;
         
+        for(int i=0;i<n;i++)
+        {
+            if(mp1.find(s[i])==mp1.end() && mp2.find(t[i])==mp2.end())
+            {
+                mp1[s[i]]=t[i];
+                mp2[t[i]]=s[i];
+            }
+            else
+            {
+                if(mp1[s[i]]!=t[i]|| mp2[t[i]]!=s[i])
+                {
+                    return false;
+                }
+                
+            }
+        }
+        return true;
         
     }
 };
