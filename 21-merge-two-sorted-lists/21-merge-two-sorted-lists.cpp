ea8cd1af327370f7ec3206288721d2498e2d1bcb
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-10-03 10:50:29 +0530

    Time: 12 ms (61.91%), Space: 14.8 MB (42.36%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -11,7 +11,6 @@
 class Solution {
 public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        
         if(!list1)
         {
             return list2;
@@ -21,16 +20,17 @@ public:
             return list1;
         }
         
-        if(list1->val<=list2->val)
+        if(list1->val <= list2->val)
         {
-            list1->next=mergeTwoLists(list1->next, list2);
+             list1->next= mergeTwoLists(list1->next,list2);
             return list1;
         }
         else
         {
-            list2->next=mergeTwoLists(list1,list2->next);
+             list2->next= mergeTwoLists(list1,list2->next);
             return list2;
         }
+            
         
     }
 };
