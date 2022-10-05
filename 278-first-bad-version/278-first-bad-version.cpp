a6bd8e6a5b984f6da1333d9946ff8c7f8ed2fac6
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-10-05 13:17:52 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (67.35%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,15 +5,13 @@ class Solution {
 public:
     int firstBadVersion(int n) {
         
-        
-        int low=1;
-        int high=n;
-        int mid;
+        long low=0;
+        long high=n;
+        long long  mid=(low+high)/2;
         
         while(low<=high)
         {
-            mid=low+(high-low)/2;
-            
+            mid=(low+high)/2;
             if(isBadVersion(mid))
             {
                 high=mid-1;
@@ -23,6 +21,8 @@ public:
                 low=mid+1;
             }
         }
+        
         return low;
+        
     }
 };
