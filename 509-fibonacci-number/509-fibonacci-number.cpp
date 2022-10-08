4058cd416ad092db0f60f8c87ca73fe731b95b78
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-10-08 06:46:58 +0530

    Time: 39 ms (6.31%), Space: 5.8 MB (95.91%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,27 +1,18 @@
 class Solution {
 public:
     
-    int solve(int n, vector<int>&vec)
+    int solve(int n)
     {
         if(n<=1)
         {
-            return vec[n]=n ;
-        }
-        if(n==2)
-        {
-            return vec[n]=1;
+            return n;
         }
         
-        if(vec[n]!=-1)
-        {
-            return vec[n];
-        }
-        return vec[n]=solve(n-1,vec)+solve(n-2,vec);
+        return solve(n-1)+solve(n-2);
     }
     
     int fib(int n) {
-        vector<int>vec(n+1, -1);
-        return solve(n, vec);
-       
+        
+        return solve(n);
     }
 };
