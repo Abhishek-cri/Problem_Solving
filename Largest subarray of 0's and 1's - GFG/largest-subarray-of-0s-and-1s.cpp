139e96678e534ee3363fd8bbd7a459f30b2e3a83
Abhishek-cri <56184705+Abhishek-cri@users.noreply.github.com>
2022-10-29 22:45:14 +0530

    Added solution - LeetHub

--- Largest subarray of 0's and 1's - GFG/largest-subarray-of-0s-and-1s.cpp
+++ Largest subarray of 0's and 1's - GFG/largest-subarray-of-0s-and-1s.cpp
@@ -17,47 +17,51 @@ class Solution{
     {
         // Your code here
     
-        unordered_map<int, int>m;
-        m[0] = -1;
+        unordered_map<int, int>mp;
+        // m[0] = -1;
         
-        int curr_sum = 0 , max_len = 0;
-        for(int i = 0; i < N; i++){
-            curr_sum += nums[i] == 0 ? -1 : 1;
+        // int curr_sum = 0 , max_len = 0;
+        // for(int i = 0; i < N; i++){
+        //     curr_sum += nums[i] == 0 ? -1 : 1;
             
-            if(m.find(curr_sum) != m.end()){
-                max_len = max(max_len , i - m[curr_sum]);
+        //     if(m.find(curr_sum) != m.end()){
+        //         max_len = max(max_len , i - m[curr_sum]);
+        //     }
+            
+        //     if(m.find(curr_sum) == m.end())
+        //         m[curr_sum] = i;
+        // }
+        
+        // return max_len;
+        int sum=0;
+        int maxi=0;
+        
+        for(int i=0;i<N;i++)
+        {
+            if(nums[i]==0)
+            {
+                nums[i]=-1;
             }
-            
-            if(m.find(curr_sum) == m.end())
-            m[curr_sum] = i;
         }
         
-        return max_len;
-        // int sum=0;
-        // int maxi=0;
-        
-        // for(int i=0;i<N;i++)
-        // {
-        //     if(nums[i]==0)
-        //     {
-        //         nums[i]=-1;
-        //     }
-        // }
-        
-        // for(int i=0;i<N;i++)
-        // {
-        //     sum+=nums[i];
+        for(int i=0;i<N;i++)
+        {
+            sum+=nums[i];
             
-        //     if(sum==0)
-        //     {
-        //         maxi=max(maxi,i+1);
-        //     }
-        //     else if(mp.find(sum)!=mp.end())
-        //     {
-        //         maxi=max(maxi,i-mp[sum]);
-        //     }
-        // }
-       
+            if(sum==0)
+            {
+                maxi=max(maxi,i+1);
+            }
+            else if(mp.find(sum)!=mp.end())
+            {
+                maxi=max(maxi,i-mp[sum]);
+            }
+            else
+            {
+                mp[sum]=i;
+            }
+        }
+       return maxi;
          
     }
 };
